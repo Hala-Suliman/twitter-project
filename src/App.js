366658c7c7e8e2ea128e17e547b7be5c2b97f788
ahesam2012 <ahesam2012@gmail.com>
2020-12-19 04:21:39 +0300

    Design

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import Sidebar from "./Sidebar"
+import Feed from "./Feed"
+import Widgets from "./Widgets"
+
+function App() {
+  return (
+
+    <div className="container flex mx-auto">
+      <Sidebar/>
+      <Feed/>
+      <Widgets/>
+    </div>
+
+    /*
+    
+    <Sidebar>
+
+    <Feed>
+
+    <Widgets>
+
+    */
+
+  );
+}
+
+export default App;
